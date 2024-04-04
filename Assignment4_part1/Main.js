058c71248790ac46241b39d411bd5f526add8d54
DevonOB <156467102+DevonOB@users.noreply.github.com>
2024-04-04 19:31:17 -0400

    rewrote part of the fiel
    my assignment wouldn't run so I had to change my method of inserting variables so that it would run.

--- Assignment4_part1/Main.js
+++ Assignment4_part1/Main.js
@@ -1,4 +1,5 @@
 
+
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -8,38 +9,42 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-storyText = "It was 94 fahrenheit outside, so " + insertX + " went for a walk. When they got to " + insertY + ", they stared in horror for a few moments, then " +insertZ +". " + setname + " saw the whole thing, but was not surprised — " +insertx + " weighs 300 pounds, and it was a hot day.";
+storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised - :insertx: weighs 300 pounds, and it was a hot day.";
 
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
-const insertY = ["the soup kitchen", "Disneyland", "the White House"]
-const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
+const insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
+const inserty = ["the soup kitchen", "Disneyland", "the White House"]
+const insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
 
 
 randomize.addEventListener('click', result);
-
+setname = "Bob"
 
 
 
 function result() {
+  let newStory = storyText;
 
-  if(customName.value !== '') {
+  const xItem = randomValueFromArray(insertx);
+  const yItem = randomValueFromArray(inserty);
+  const zItem = randomValueFromArray(insertz);
+
+  
+  newStory = newStory.replaceAll(':insertx:',xItem);
+  newStory = newStory.replaceAll(':inserty:',yItem);
+  newStory = newStory.replaceAll(':insertz:',zItem);
+
+  if (customName.value !== '') {
     const name = customName.value;
-    setname = name;
+    newStory = newStory.replaceAll('Bob', name);
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + "stones";
-    const temperature =  Math.round((94-32) * (5/9)) + "Celsius";
-
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
   }
 
-  xItem = randomValueFromArray(insertX);
-  yItem = randomValueFromArray(insertY);
-  zItem = randomValueFromArray(insertZ);
-
-
-  newStory = storyText;
-  newStory = "It was "+temperature+" outside, so " + xItem + " went for a walk. When they got to " + yItem + ", they stared in horror for a few moments, then " +zItem +". " + setname + " saw the whole thing, but was not surprised — " +xItem + " weighs "+weight+", and it was a hot day.";
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
