23bfca65dd7590ef0618a87595b8c875db29e2b1
DevonOB <156467102+DevonOB@users.noreply.github.com>
2024-04-04 19:48:14 -0400

    added the arrays with image names
    I added the array with the image source as well as the array with the image alternative text.

--- Assignment4_part2/gallery-start/gallery-start/main.js
+++ Assignment4_part2/gallery-start/gallery-start/main.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
+const alt_descriptions = ["Closeup of an eye", "Patterened rock wall", "Purple and white flowers", "Ancient Egyptian hyroglifics", "Moth on leaf"]
 
 /* Looping through images */
 
