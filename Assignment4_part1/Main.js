36ab4038010defe8d50c8fb96ad006e86ae15f55
DevonOB <156467102+DevonOB@users.noreply.github.com>
2024-04-04 18:28:09 -0400

    added temperature and weight
    added calculations for temperature to Celsius and weight the stones. also added custom name.

--- Assignment4_part1/Main.js
+++ Assignment4_part1/Main.js
@@ -8,27 +8,38 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+storyText = "It was 94 fahrenheit outside, so " + insertX + " went for a walk. When they got to " + insertY + ", they stared in horror for a few moments, then " +insertZ +". " + setname + " saw the whole thing, but was not surprised — " +insertx + " weighs 300 pounds, and it was a hot day.";
 
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
 const insertY = ["the soup kitchen", "Disneyland", "the White House"]
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
 
+
 randomize.addEventListener('click', result);
+
+
+
 
 function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    setname = name;
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300/14) + "stones";
+    const temperature =  Math.round((94-32) * (5/9)) + "Celsius";
 
   }
 
-  story.textContent = ;
+  xItem = randomValueFromArray(insertX);
+  yItem = randomValueFromArray(insertY);
+  zItem = randomValueFromArray(insertZ);
+
+
+  newStory = storyText;
+  newStory = "It was "+temperature+" outside, so " + xItem + " went for a walk. When they got to " + yItem + ", they stared in horror for a few moments, then " +zItem +". " + setname + " saw the whole thing, but was not surprised — " +xItem + " weighs "+weight+", and it was a hot day.";
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
